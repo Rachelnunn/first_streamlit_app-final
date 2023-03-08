5a6a389befb138e57d90021cb4896d56610d022e
Rachelnunn <127329661+Rachelnunn@users.noreply.github.com>
2023-03-08 16:42:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text('Eggs benedict')
 streamlit.text('Full English')
 
 streamlit.header('Build your own smoothie!')
+
+import pandas
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
