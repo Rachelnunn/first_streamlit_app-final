7e82357908b0490d9e264c5514823eef36a09b17
Rachelnunn <127329661+Rachelnunn@users.noreply.github.com>
2023-03-08 16:43:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.text('Full English')
 streamlit.header('Build your own smoothie!')
 
 import pandas
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
