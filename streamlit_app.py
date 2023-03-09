f1707903b61974c46164b7fccd16abc22f38a74b
Rachelnunn <127329661+Rachelnunn@users.noreply.github.com>
2023-03-09 13:36:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError streamlit.title('My Parents New Healthy Dinner') streamlit.header('Breakfast Menu')
+from urllib.error import URLError 
+streamlit.title('My Parents New Healthy Dinner') 
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
@@ -10,7 +12,8 @@ streamlit.text('🥑🍞 Avocado Toast') streamlit.header('🍌🥭 Build Your 
 my_fruit_list = my_fruit_list.set_index('Fruit') # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected] # Display the table on the page.
-streamlit.dataframe(fruits_to_show) streamlit.header("Fruityvice Fruit Advice!")
+streamlit.dataframe(fruits_to_show) 
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
